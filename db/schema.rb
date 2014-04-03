821aae2bfbccb9f849f353fe3b90f495bc880f6a
leslicita_mc@hotmail.com <leslicita_mc@hotmail.com>
2014-04-03 03:34:40 -0400

    Likes creados, aun no enlazados

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403065125) do
+ActiveRecord::Schema.define(version: 20140403072737) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20140403065125) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "likes", force: true do |t|
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
