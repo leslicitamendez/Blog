004aa7fdf06154c8d30f31479fc808e253c9c42e
leslicita_mc@hotmail.com <leslicita_mc@hotmail.com>
2014-04-02 23:20:41 -0400

    Se creo el blog vacio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f7c78d65b2287c4c594056b84bcb4dade7c3b2b2cd0feb5d7b4699783bbc5e3a42d397782d260e4a5e6543e1b821b5ddd6e00902b5da62f7ba698ff7b9e57120'
