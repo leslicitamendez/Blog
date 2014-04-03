98e8ae7da43aa9e8c07e3b00ad35cb6a70f28e35
leslicita_mc@hotmail.com <leslicita_mc@hotmail.com>
2014-04-03 03:15:04 -0400

    Comentarios anhadidos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Blog::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :posts do
+    resources :comments
+  end
+
   get '/blog' => 'blog#index'
 
   get '/posts' => 'posts#index'
